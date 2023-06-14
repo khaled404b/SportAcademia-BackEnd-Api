417fcc3d6250196682e7fed9f314b80400e84d4d
khaled <khaledzaben404@gmail.com>
2023-06-14 03:17:17 +0300

    axzzzz

--- middleware/auth/authMiddleware.js
+++ middleware/auth/authMiddleware.js
@@ -5,6 +5,11 @@ const jwtSecret = process.env.JWT_SECRET;
 
 const authMiddleware = async (req, res, next) => {
   const authHeader = req.headers.authorization;
+  
+  if (!authHeader) {
+    return res.status(401).send('Authorization header is required');
+  }
+
   const idToken = authHeader.replace("Bearer ", "");
 
   try {
@@ -27,43 +32,6 @@ const authMiddleware = async (req, res, next) => {
   }
 };
 
-
-// const isAuthenticated = (req, res, next) => {
-//   const authHeader = req.header('Authorization');
-//   if (!authHeader) {
-//     return res.status(401).json({ message: 'No token provided' });
-//   }
-
-//   const token = authHeader.split(' ')[1];
-
-//   if (!token) {
-//     return res.status(401).json({ message: 'No token provided' });
-//   }
-
-//   try {
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     req.user = decoded;
-//     next();
-//   } catch (error) {
-//     res.status(401).json({ message: 'Invalid token' });
-//   }
-// };
-
-
-// const isAdmin = (req, res, next) => {
-//   if (req.user.role === 'admin') {
-//     next();
-//   } else {
-//     res.status(403).json({ message:'Forbidden: You do not have permission to perform this action' });
-//   }
-//   };
-
-
-
 module.exports = {
-  // isAuthenticated,
-  // isAdmin,
   authMiddleware,
-
-
 };
